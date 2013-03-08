5022ab34cb784b6f11138ae0e33b36ef7f3a9efa
Sapronov Alex <warmonger72@gmail.com>
2013-03-08 11:48:41 +0700

    [UPDATE-VER] Update ver to pre-alpha-3

--- src/defines/defines.h
+++ src/defines/defines.h
@@ -1,8 +1,8 @@
 #ifndef __DEFINES_H
 #define __DEFINES_H
 
-#define GL_PROG_VERSION_STR	"pre-alpha-2.5"
-#define GL_PROG_BUILD_STR	"2013.02.11"
+#define GL_PROG_VERSION_STR	"pre-alpha-3"
+#define GL_PROG_BUILD_STR	"2013.03.08"
 #define GL_PROG_NAME            "QSopherim"
 #define GL_PROG_AUTHOR          "Sapronov Alexander"
 #define GL_WEB_SITE             "http://warmonger1.github.com/QSopherim"
